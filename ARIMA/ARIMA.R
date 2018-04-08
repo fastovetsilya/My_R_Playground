72f2edbf36e4135ff9678a48629b58b07d4e1108
fastovetsilya <you@example.com>
2018-04-08 13:40:52 +0300

    minor

--- ARIMA/ARIMA.R
+++ ARIMA/ARIMA.R
@@ -9,12 +9,13 @@ fit_arima <- auto.arima(Data, d = NA, D = NA, max.p = Max_params, max.q = Max_pa
                         max.d = Max_params, max.D = Max_params, 
                         start.p = 1,
                         start.q = 1, start.P = 1, start.Q = 1, stationary = FALSE,
-                        seasonal = TRUE, ic = 'aic', stepwise = FALSE,
+                        seasonal = FALSE, ic = 'aic', stepwise = FALSE,
                         trace = TRUE, approximation = FALSE,
                         truncate = NULL, xreg = NULL, test = 'kpss',
                         seasonal.test = "ocsb", allowdrift = TRUE, allowmean = TRUE,
                         lambda = NULL, biasadj = FALSE, parallel = TRUE, num.cores = 2)
-plot(forecast(fit_arima, h = 100), xlim = c(220, nrow(Data)+20), 
-     ylim = c(40, 60))
+plot(forecast(fit_arima, h = 100), xlim = c(210, nrow(Data)+20), 
+     ylim = c(200, 260))
 fit_nnetar <- nnetar(Data[,1], size=20, repeats = 20)
-plot(forecast(fit_nnetar, h = 100))
+plot(forecast(fit_nnetar, h = 30))
+  
