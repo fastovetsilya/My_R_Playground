74bb0add0cecec6a2f6c43e01a1033a6a2c97e7b
Ilya <ilya.fastovets@gmail.com>
2021-09-03 14:45:46 -0700

    Chapter 5.1

--- Rethinking Statistics/5.R
+++ Rethinking Statistics/5.R
@@ -72,6 +72,143 @@ DMA_dag1 <- dagitty("dag{D <- A -> M -> D}")
 impliedConditionalIndependencies(DMA_dag1)
 
 
+### Multiple regression: 5.10-5.27
+# Approximate posterior with quap
+m5.3 <- quap(
+  alist(
+    D ~ dnorm(mu, sigma), 
+    mu <- a + bM * M + bA * A, 
+    a ~ dnorm(0, 0.2), 
+    bM ~ dnorm(0, 0.5), 
+    bA ~ dnorm(0, 0.5), 
+    sigma ~ dexp(1)
+  ), data = d)
+precis(m5.3)
+
+# Plot
+plot(coeftab(m5.1, m5.2, m5.3), par = c("bA", "bM"))
+
+# Simulating the divorce example
+N <- 50
+age <- rnorm(N)
+mar <- rnorm(N, -age)
+div <- rnorm(N, age)
+
+# Predictor residual plots
+# Fit the model
+m5.4 <- quap(
+  alist(
+    M ~ dnorm(mu, sigma), 
+    mu <- a + bAM * A, 
+    a ~ dnorm(0, 0.2), 
+    bAM ~ dnorm(0, 0.5), 
+    sigma ~ dexp(1)
+  ), data = d)
+# Compute residuals
+mu <- link(m5.4)
+mu_mean <- apply(mu, 2, mean)
+mu_resid <- d$M - mu_mean
+
+# Posterior predictive check
+# Simulate predictions, averaging over posterior
+# Call link without specifying new data, so it uses original data
+mu <- link(m5.3)
+# Summarize samples across cases 
+mu_mean <- apply(mu, 2, mean)
+mu_PI <- apply(mu, 2, PI)
+# Simulate observations
+# Again no new data, use original data
+D_sim <- sim(m5.3, n=1e4)
+D_PI <- apply(D_sim, 2, PI)
+# Plot predictions against observed
+plot(mu_mean ~ d$D, col = rangi2, ylim = range(mu_PI), 
+     xlab = "Observed divorce", ylab = "Predicted divorce")
+abline(a = 0, b = 1, lty = 2)
+for(i in 1:nrow(d)) lines(rep(d$D[i], 2), mu_PI[, i], col = rangi2)
+# Identify the outlier states
+identify(x = d$D, y = mu_mean, labels = d$Loc)
+
+# Simulate spurious association
+N <- 100
+x_real <- rnorm(N)
+x_spur <- rnorm(N, x_real)
+y <- rnorm(N, x_real)
+d <- data.frame(y, x_real, x_spur)
+
+# Counterfactual plots
+data(WaffleDivorce)
+d <- list()
+d$A <- standardize(WaffleDivorce$MedianAgeMarriage)
+d$D <- standardize(WaffleDivorce$Divorce)
+d$M <- standardize(WaffleDivorce$Marriage)
+# Run two regressions at the same time
+m5.3_A <- quap(
+  alist(
+    ## A -> D <- M
+    D ~ dnorm(mu, sigma), 
+    mu <- a + bM * M + bA * A, 
+    a ~ dnorm(0, 0.2), 
+    bM ~ dnorm(0, 0.5), 
+    bA ~ dnorm(0, 0.5), 
+    sigma ~ dexp(1), 
+    ## A -> M
+    M ~ dnorm(mu_M, sigma_M), 
+    mu_M <- aM + bAM * A, 
+    aM <- dnorm(0, 0.2), 
+    bAM ~ dnorm(0, 0.5), 
+    sigma_M ~ dexp(1)
+  ), data = d)
+precis(m5.3_A)
+
+# Manipulate A
+A_seq <- seq(from = -2, to = 2, length.out = 30)
+# Prep data
+sim_dat <- data.frame(A = A_seq)
+# Simulate M and then D, using A_seq
+s <- sim(m5.3_A, data = sim_dat, vars = c("M", "D"))
+# Plot the predictions
+plot(sim_dat$A, colMeans(s$D), ylim = c(-2, 2), type = "l", 
+     xlab = "Manipulated A", ylab = "Counterfactual D")
+shade(apply(s$D, 2, PI), sim_dat$A)
+mtext("Total counterfactual effect of A on D")
+
+# Expected causal effect of increasing median age at marriage from 20 to 30
+# New data frame, standardized to mean 26.1 and std dev 1.24
+sim2_dat <- data.frame(A = (c(20, 30) - 26.1) / 1.24)
+s2 <- sim(m5.3_A, data = sim2_dat, vars = c("M", "D"))
+mean(s2$D[, 2] - s2$D[, 1])
+
+# Simulate a counterfactual for an average state A = 0, and see what changing M does
+sim_dat <- data.frame(M = seq(from = -2, to = 2, length.out = 30), A = 0)
+s <- sim(m5.3_A, data = sim_dat, vars = "D")
+
+plot(sim_dat$M, colMeans(s), ylim = c(-2, 2), type = "l", 
+     xlab = "manipulated M", ylab = "counterfactual D")
+shade(apply(s, 2, PI), sim_dat$M)
+mtext("Total counterfactual effects of M on D")
+
+# Simulating counterfactuals
+# Define a range of values to assign to A
+A_seq <- seq(from = -2, to = 2, length.out = 30)
+# Extract the posterior samples
+post <- extract.samples(m5.3_A)
+M_sim <- with(post, sapply(1:30, 
+              function(i) rnorm(1e3, aM + bAM * A_seq[i], sigma_M)))
+# Simulate D
+D_sim <- with(post, sapply(1:30, 
+                           function(i) rnorm(1e3, a + bA * A_seq[i] + bM * M_sim[, i], sigma)))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
